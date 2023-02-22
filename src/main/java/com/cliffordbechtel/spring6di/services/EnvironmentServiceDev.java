93034278291ba9b9188d3b35ac7e8544dca981ba
Clifford <crbechtel@gmail.com>
2023-02-21 23:03:20 -0500

    assign-di

---
+++ src/main/java/com/cliffordbechtel/spring6di/services/EnvironmentServiceDev.java
@@ -0,0 +1,16 @@
+package com.cliffordbechtel.spring6di.services;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+@Profile({"dev", "default"})
+@Service
+public class EnvironmentServiceDev implements EnvironmentService {
+    @Override
+    public String getEnv() {
+        return "dev";
+    }
+}
